aeaab19c0be32689714ffd74c1fb15fc3b54dc4e
Bjarke Lindberg <bvli@beverli.net>
2018-02-04 16:34:05 +0100

    Change name and type of the calculator
    Because time ran from static classes.

---
+++ src/B.Calendar/HolidayCalculator.cs
@@ -0,0 +1,202 @@
+﻿namespace B.Calendar
+{
+    using System;
+
+    /// <summary>
+    /// Calculates Christian holidays based on the Gregorian calendar.
+    /// </summary>
+    public class HolidayCalculator
+    {
+        /// <summary>
+        /// Gets the carnival sunday
+        /// </summary>
+        /// <param name="year">The year.</param>
+        /// <returns></returns>
+        public DateTime GetCarnival(int year)
+        {
+            if (0 > year || year > DateTime.MaxValue.Year)
+            {
+                throw new ArgumentOutOfRangeException(nameof(year));
+            }
+
+            return CalculateEasterSunday(year).AddDays(-49);
+        }
+
+        /// <summary>
+        /// Gets the palm sunday of the specified <paramref name="year"/>.
+        /// </summary>
+        /// <param name="year">The year.</param>
+        /// <returns></returns>
+        /// <remarks>Sunday before Easter.</remarks>
+        public DateTime GetPalmSunday(int year)
+        {
+            if (0 > year || year > DateTime.MaxValue.Year)
+            {
+                throw new ArgumentOutOfRangeException(nameof(year));
+            }
+
+            return CalculateEasterSunday(year).AddDays(-7);
+        }
+
+        /// <summary>
+        /// Gets the maundy thursday of the specified <paramref name="year"/>.
+        /// </summary>
+        /// <param name="year">The year.</param>
+        /// <returns></returns>
+        /// <remarks>The Thursday before Easter; commemorates the Last Supper.</remarks>
+        public DateTime GetMaundyThursday(int year)
+        {
+            if (0 > year || year > DateTime.MaxValue.Year)
+            {
+                throw new ArgumentOutOfRangeException(nameof(year));
+            }
+
+            return CalculateEasterSunday(year).AddDays(-3);
+        }
+
+        /// <summary>
+        /// Gets the good friday of the specified <paramref name="year"/>.
+        /// </summary>
+        /// <param name="year">The year.</param>
+        /// <returns></returns>
+        /// <remarks>Friday before Easter.</remarks>
+        public DateTime GetGoodFriday(int year)
+        {
+            if (0 > year || year > DateTime.MaxValue.Year)
+            {
+                throw new ArgumentOutOfRangeException(nameof(year));
+            }
+
+            return GetMaundyThursday(year).AddDays(1);
+        }
+
+        /// <summary>
+        /// Gets the easter monday of the specified <paramref name="year"/>.
+        /// </summary>
+        /// <param name="year">The year.</param>
+        /// <returns></returns>
+        /// <remarks></remarks>
+        public DateTime GetEasterMonday(int year)
+        {
+            if (0 > year || year > DateTime.MaxValue.Year)
+            {
+                throw new ArgumentOutOfRangeException(nameof(year));
+            }
+
+            return CalculateEasterSunday(year).AddDays(1);
+        }
+
+        /// <summary>
+        /// Gets the great prayer day of the specified <paramref name="year"/>.
+        /// </summary>
+        /// <param name="year">The year.</param>
+        /// <returns></returns>
+        /// <remarks>This is a specific danish holyday.</remarks>
+        public DateTime GetGreatPrayerDay(int year)
+        {
+            if (0 > year || year > DateTime.MaxValue.Year)
+            {
+                throw new ArgumentOutOfRangeException(nameof(year));
+            }
+
+            // fourth friday after easter.
+            return CalculateEasterSunday(year).AddDays(5 + 3 * 7);
+        }
+
+        /// <summary>
+        /// Gets the ascension day of the specified <paramref name="year"/>.
+        /// </summary>
+        /// <param name="year">The year.</param>
+        /// <returns></returns>
+        /// <remarks>Celebration of the Ascension of Christ into heaven; observed on the 40th day after Easter</remarks>
+        public DateTime GetAscensionDay(int year)
+        {
+            if (0 > year || year > DateTime.MaxValue.Year)
+            {
+                throw new ArgumentOutOfRangeException(nameof(year));
+            }
+
+            // sixth thursday after easter.
+            return CalculateEasterSunday(year).AddDays(39);
+        }
+
+        /// <summary>
+        /// Gets the whit sunday of the specified <paramref name="year"/>.
+        /// </summary>
+        /// <param name="year">The year.</param>
+        /// <returns></returns>
+        /// <remarks>Seventh Sunday after Easter; commemorates the emanation of the Holy Spirit to the Apostles; a quarter day in Scotland.</remarks>
+        public DateTime GetWhitSunday(int year)
+        {
+            if (0 > year || year > DateTime.MaxValue.Year)
+            {
+                throw new ArgumentOutOfRangeException(nameof(year));
+            }
+
+            return CalculateEasterSunday(year).AddDays(7 * 7);
+        }
+
+        /// <summary>
+        /// Gets the whit monday of the specified <paramref name="year"/>.
+        /// </summary>
+        /// <param name="year">The year.</param>
+        /// <returns></returns>
+        /// <remarks>The day after Whitsunday; a legal holiday in England and Wales and Ireland.</remarks>
+        public DateTime GetWhitMonday(int year)
+        {
+            if (0 > year || year > DateTime.MaxValue.Year)
+            {
+                throw new ArgumentOutOfRangeException(nameof(year));
+            }
+
+            return GetWhitSunday(year).AddDays(1);
+        }
+
+
+        /// <summary>
+        /// Gets ash wednesday.
+        /// </summary>
+        /// <param name="year">The year.</param>
+        /// <returns></returns>
+        public DateTime GetAshWednesday(int year)
+        {
+            if (0 > year || year > DateTime.MaxValue.Year)
+            {
+                throw new ArgumentOutOfRangeException(nameof(year));
+            }
+
+            return CalculateEasterSunday(year).AddDays(-46);
+        }
+
+        /// <summary>
+        /// Calculates easter sunday for the specified <paramref name="year"/>.
+        /// </summary>
+        /// <param name="year">The year.</param>
+        /// <returns>The <see cref="DateTime">date</see> of easter sunday.</returns>
+        /// <remarks>This method uses the algorithm specified in the wikipedia article: <a href="http://en.wikipedia.org/wiki/Computus">Computus</a>.</remarks>
+        public DateTime CalculateEasterSunday(int year)
+        {
+            if (0 > year || year > DateTime.MaxValue.Year)
+            {
+                throw new ArgumentOutOfRangeException(nameof(year));
+            }
+
+            var a = year % 19;
+            var b = year / 100;
+            var c = year % 100;
+            var d = b / 4;
+            var e = b % 4;
+            var f = (b + 8) / 25;
+            var g = (b - f + 1) / 3;
+            var h = (19 * a + b - d - g + 15) % 30;
+            var i = c / 4;
+            var k = c % 4;
+            var l = (32 + 2 * e + 2 * i - h - k) % 7;
+            var m = (a + 11 * h + 22 * l) / 451;
+            var month = (h + l - 7 * m + 114) / 31;
+            var day = ((h + l - 7 * m + 114) % 31) + 1;
+
+            return new DateTime(year, month, day).Date;
+        }
+    }
+}
---
+++ test/B.Calendar.Test/HolidayCalculatorTest.cs
@@ -0,0 +1,105 @@
+﻿namespace B.Calendar.Test
+{
+    using System;
+    using Xunit;
+
+    public class HolidayCalculatorTest
+    {
+        private readonly HolidayCalculator _sut = new HolidayCalculator();
+
+        [Fact]
+        public void EasterTest()
+        {
+            var easter2008 = new DateTime(2008, 3, 23);
+            var easter1961 = new DateTime(1961, 4, 2);
+            var easter2007 = new DateTime(2007, 4, 8);
+
+            Assert.Equal(easter2008, _sut.CalculateEasterSunday(2008));
+            Assert.Equal(easter1961, _sut.CalculateEasterSunday(1961));
+            Assert.Equal(easter2007, _sut.CalculateEasterSunday(2007));
+        }
+
+        [Fact]
+        public void CarnivalTest()
+        {
+            var carnival2008 = new DateTime(2008, 2, 3);
+            Assert.Equal(carnival2008, _sut.GetCarnival(2008));
+        }
+
+        [Fact]
+        public void MaundyThursdayTest()
+        {
+            var maundyThursday2008 = new DateTime(2008, 3, 20);
+            Assert.Equal(DayOfWeek.Thursday, maundyThursday2008.DayOfWeek);
+            Assert.Equal(maundyThursday2008, _sut.GetMaundyThursday(2008));
+        }
+
+        [Fact]
+        public void GoodFridayTest()
+        {
+            var goodFriday2008 = new DateTime(2008, 3, 21);
+            Assert.Equal(DayOfWeek.Friday, goodFriday2008.DayOfWeek);
+            Assert.Equal(goodFriday2008, _sut.GetGoodFriday(2008));
+        }
+
+        [Fact]
+        public void PalmSundayTest()
+        {
+            var palmSunday2008 = new DateTime(2008, 3, 16);
+            Assert.Equal(DayOfWeek.Sunday, palmSunday2008.DayOfWeek);
+            Assert.Equal(palmSunday2008, _sut.GetPalmSunday(2008));
+        }
+
+        [Fact]
+        public void EasterMondayTest()
+        {
+            var easterMonday2008 = new DateTime(2008, 3, 24);
+            Assert.Equal(DayOfWeek.Monday, easterMonday2008.DayOfWeek);
+            Assert.Equal(easterMonday2008, _sut.GetEasterMonday(2008));
+        }
+
+        [Fact]
+        public void GreatPrayerDayTest()
+        {
+            var greatPrayer2008 = new DateTime(2008, 4, 18);
+            Assert.Equal(DayOfWeek.Friday, greatPrayer2008.DayOfWeek);
+            Assert.Equal(greatPrayer2008, _sut.GetGreatPrayerDay(2008));
+        }
+
+        [Fact]
+        public void AscensionDayTest()
+        {
+            var ascension2008 = new DateTime(2008, 5, 1);
+            Assert.Equal(DayOfWeek.Thursday, ascension2008.DayOfWeek);
+            Assert.Equal(ascension2008, _sut.GetAscensionDay(2008));
+        }
+
+        [Fact]
+        public void WhitSundayTest()
+        {
+            var whitSunday2008 = new DateTime(2008, 5, 11);
+            Assert.Equal(DayOfWeek.Sunday, whitSunday2008.DayOfWeek);
+            Assert.Equal(whitSunday2008, _sut.GetWhitSunday(2008));
+        }
+
+        [Fact]
+        public void WhitMondayTest()
+        {
+            var whitMonday2008 = new DateTime(2008, 5, 12);
+            Assert.Equal(DayOfWeek.Monday, whitMonday2008.DayOfWeek);
+            Assert.Equal(whitMonday2008, _sut.GetWhitMonday(2008));
+        }
+
+        [Fact]
+        public void AshWednesdayTest()
+        {
+            var ashWednesday2008 = new DateTime(2008, 2, 6);
+            var ashWednesday2007 = new DateTime(2007, 2, 21);
+            Assert.Equal(DayOfWeek.Wednesday, ashWednesday2008.DayOfWeek);
+            Assert.Equal(DayOfWeek.Wednesday, ashWednesday2007.DayOfWeek);
+
+            Assert.Equal(ashWednesday2008, _sut.GetAshWednesday(2008));
+            Assert.Equal(ashWednesday2007, _sut.GetAshWednesday(2007));
+        }
+    }
+}
